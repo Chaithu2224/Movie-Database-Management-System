05927b4082e570cd2b7fad61df436dfa7f4566c8
AllamSindhuja <145509558+AllamSindhuja@users.noreply.github.com>
2024-10-22 20:52:36 -0500

    removed frontend code

--- MovieTicketBooking/MovieTicketBooking/frontend/src/utils/formatRuntime.js
+++
@@ -1,7 +0,0 @@
-const FormatRuntime = (runtime) => {
-  const hours = Math.floor(runtime / 60);
-  const minutes = runtime % 60;
-  return `${hours}h ${minutes}min`;
-};
-
-export default FormatRuntime;
